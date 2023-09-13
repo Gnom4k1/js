945ac710b0aa2011eda86e038c38f609dddc4fe2
Rayan_Osling <100954683+Gnom4k1@users.noreply.github.com>
2023-09-13 19:50:41 +0300

    yh
    yh

---
+++ js.js
@@ -0,0 +1,40 @@
+let name = "Джон"
+let admin = name
+alert(name)
+let planet1 = "Earth"
+let eye = "Черный"
+let userName = "Вася"
+let user = userName
+console.log(user)
+let num = 10
+prompt(num)
+let a = 15
+let b = 8
+alert(a + b)
+alert(a - b)
+alert(a * b)
+alert(a / b)
+let c = 35
+let d = 5
+let result = a + d
+alert(result)
+let a1 = 10
+let b1 = 22
+let c1 = 5
+alert(a1+b1+c1)
+let a2 = 77
+let b2 = 20
+let c2 = a2 - b2
+let d1 = 7
+let result1 = c2 + d1
+prompt("Значение вычисления: " , result1)
+let str = "Hello, World!"
+alert(str)
+let str1 = "Hello "
+let str2 = "World!"
+alert(str1 + str2)
+let name1 = "Имя"
+alert(`Привет,  %${name1}%!`)
+let age = "Возраст"
+alert(`Мне %${name1}% лет!`)
+let name2 = promt("Ваше имя?")
